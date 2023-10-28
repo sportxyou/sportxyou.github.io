3cade1268581f647b80208cf7061a38d3dc54704
sportxyou <143332422+sportxyou@users.noreply.github.com>
2023-10-28 07:42:12 +0700

    Create corsx.php

---
+++ anti-cors/corsx.php
@@ -0,0 +1,5 @@
+<?php
+// Set header untuk CORS - mengizinkan akses dari semua domain
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
