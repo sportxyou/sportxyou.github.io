33c895cda620a402b06a21871cccf30a24674112
sportxyou <143332422+sportxyou@users.noreply.github.com>
2023-11-09 15:09:55 +0700

    Update v13quran2023.js

--- quran/js/v13quran2023.js
+++ quran/js/v13quran2023.js
@@ -1,23 +1,59 @@
 var app = angular.module('QuranApp', []);
 
-app.controller('QuranController', function($scope, $http) {
+app.controller('QuranController', function ($scope, $http) {
+    $scope.hadithData = {}; // Simpan data hadis di sini
+    $scope.currentPage = 1; // Inisialisasi currentPage di dalam $scope
+    var limit = 13;
+    var pagesLimit = 10;
     $scope.loading = true;
     $scope.preBismillah = {
-  audio: {
-    primary1: 'https://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/1' // Ganti URL dengan URL audio yang tepat
-  }
-};
-let currentAudio = null;
-$scope.tafsirData = {};
-// $scope.surahList = [];
+        audio: {
+            primary1: 'https://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/1' // Ganti URL dengan URL audio yang tepat
+        }
+    };
+    let currentAudio = null;
+    $scope.tafsirData = {};
+    $scope.selectedAyat = {}; 
+    $scope.selectedNoAyat = {};
 
-  // Fungsi untuk memuat data surah saat pilihan berubah
-  // $scope.$watch('selectedSurah', function() {
-  //   if ($scope.selectedSurah) {
-      
-      
-      var apiUrl = `https://api.quran.gading.dev/surah/${surah_no}`;
-  
+    function loadHadithData(page) {
+        $http.get(`https://hadis-api-id.vercel.app/hadith/${tokoh}/?page=${page}&limit=${limit}`)
+            .then(function (response) {
+                $scope.hadithData = response.data; // Memasukkan data ke dalam $scope
+                $scope.currentPage = page; // Menggunakan $scope.currentPage
+            }, function (error) {
+                console.error('Gagal mengambil data:', error);
+            });
+    }
+
+    loadHadithData($scope.currentPage); // Menggunakan $scope.currentPage
+
+    $scope.loadPage = function (page) {
+        if (page >= 1 && page <= $scope.hadithData.pagination.totalPages) {
+            loadHadithData(page);
+        }
+    };
+
+    $scope.getPages = function () {
+        if ($scope.hadithData.pagination) {
+            $scope.totalPages = $scope.hadithData.pagination.totalPages;
+            var start = Math.max(1, $scope.currentPage - Math.floor(pagesLimit / 2));
+            var end = Math.min(start + pagesLimit - 1, $scope.totalPages);
+
+            if (end - start + 1 < pagesLimit) {
+                start = Math.max(1, end - pagesLimit + 1);
+            }
+
+            var pagesArray = [];
+            for (var i = start; i <= end; i++) {
+                pagesArray.push(i);
+            }
+            return pagesArray;
+        }
+    };
+
+    
+  var apiUrl = `https://api.quran.gading.dev/surah/${surah_no}`;
   
   $http.get(apiUrl)
     .then(function(response) {
@@ -31,8 +67,8 @@ $scope.tafsirData = {};
             juz: ayat.meta && ayat.meta.juz ? ayat.meta.juz : 'Data Tidak Tersedia',
             teksArab: ayat.text && ayat.text.arab ? ayat.text.arab : 'Data Tidak Tersedia',
             transliterasiInggris: ayat.text && ayat.text.transliteration && ayat.text.transliteration.en ? ayat.text.transliteration.en : 'Data Tidak Tersedia',
-            terjemahanEn: ayat.translation && ayat.translation.id ? ayat.translation.en : 'Data Tidak Tersedia',
             terjemahan: ayat.translation && ayat.translation.id ? ayat.translation.id : 'Data Tidak Tersedia',
+            terjemahanEn: ayat.translation && ayat.translation.id ? ayat.translation.en : 'Data Tidak Tersedia',
             audio: ayat.audio && ayat.audio.primary ? ayat.audio.primary : 'Data Tidak Tersedia',
             tafsirSingkat: ayat.tafsir && ayat.tafsir.id && ayat.tafsir.id.short ? ayat.tafsir.id.short : 'Data Tidak Tersedia',
             tafsirPanjang: ayat.tafsir && ayat.tafsir.id && ayat.tafsir.id.long ? ayat.tafsir.id.long : 'Data Tidak Tersedia',
@@ -72,7 +108,7 @@ $scope.tafsirData = {};
     $scope.playAudio = function(data) {
 if (currentAudio) {
     currentAudio.pause();
-    currentAudio.currentTime = 0; // Mengembalikan waktu audio ke awal saat dihentikan
+    currentAudio.currentTime = 0; 
 }
 
 if (!data.audioPlayer) {
@@ -82,7 +118,7 @@ if (!data.audioPlayer) {
 } else {
     if (!data.audioPlayer.paused) {
         data.audioPlayer.pause();
-        data.audioPlayer.currentTime = 0; // Mengembalikan waktu audio ke awal saat dihentikan
+        data.audioPlayer.currentTime = 0; 
     } else {
         data.audioPlayer.play();
         currentAudio = data.audioPlayer;
@@ -93,18 +129,17 @@ if (!data.audioPlayer) {
  // Fungsi untuk mengubah nomor surah sesuai kebutuhan
 $scope.formatSurahNumber = function(number) {
     if (number < 10) {
-        return '00' + number; // menambahkan "00" di depan angka 1-9
+        return '00' + number;
     } else if (number >= 10 && number < 100) {
-        return '0' + number; // menambahkan "0" di depan angka 10-99
+        return '0' + number; 
     } else {
-        return number; // untuk ratusan, biarkan sesuai dengan angka asli
+        return number; 
     }
 };
     
 // conver to arabic
 $scope.convertToArabic = function(number) {
   // Fungsi untuk mengonversi angka ke angka Arab
-  // Contoh sederhana, Anda mungkin membutuhkan fungsi yang lebih lengkap
   const arabicNumbers = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
   const converted = number.toString().replace(/\d/g, (d) => arabicNumbers[d]);
   return converted;
