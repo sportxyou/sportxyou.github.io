5f04bc3cf7429a301b609312f4b996bf0a7cd39e
sportxyou <143332422+sportxyou@users.noreply.github.com>
2023-11-28 03:15:21 +0700

    Update al-quran.js

--- quran/js/al-quran.js
+++ quran/js/al-quran.js
@@ -1,106 +1,128 @@
 var app = angular.module('QuranApp', []);
 
-        app.filter('formatNumber', function () {
-            return function (input) {
-                if (input < 10) {
-                    return '00' + input;
-                } else if (input < 100) {
-                    return '0' + input;
-                } else {
-                    return input;
-                }
-            };
-        });
+    app.filter('formatNumber', function () {
+        return function (input) {
+            if (input < 10) {
+                return '00' + input;
+            } else if (input < 100) {
+                return '0' + input;
+            } else {
+                return input;
+            }
+        };
+    });
 
-        app.controller('QuranController', function ($scope, $http, $window) {
-            // Mendapatkan nama surah dari parameter URL dengan karakter "-" diganti dengan "="
-var urlParams = new URLSearchParams(window.location.search);
-var surahNameWithDash = urlParams.get('surah');
-var surahName = surahNameWithDash ? surahNameWithDash.replace(/=/g, "-") : null;
+    app.controller('QuranController', function ($scope, $http, $window, $timeout) {
+        var urlParams = new URLSearchParams(window.location.search);
+        var surahNameWithDash = urlParams.get('surah');
+        var surahName = surahNameWithDash ? surahNameWithDash.replace(/=/g, "-") : null;
 
-            // Mengambil data surah berdasarkan nama
-            $http.get('https://apidataislamic.github.io/data/quran.json').then(function (response) {
-                $scope.ayahs = response.data.data; 
-                $scope.selectedAyah = null;
+        $http.get('https://apidataislamic.github.io/data/quran.json').then(function (response) {
+            $scope.ayahs = response.data.data; 
+            $scope.selectedAyah = null;
+            $scope.previousSurah = null;
+            $scope.nextSurah = null;
 
-                // Mendapatkan nama surah dari parameter URL dengan karakter "-" diganti dengan "="
-                var urlParams = new URLSearchParams(window.location.search);
-                var surahNameWithDash = urlParams.get('surah');
-                var surahName = surahNameWithDash ? surahNameWithDash.replace(/=/g, "-") : null;
+            var urlParams = new URLSearchParams(window.location.search);
+            var surahNameWithDash = urlParams.get('surah');
+            var surahName = surahNameWithDash ? surahNameWithDash.replace(/=/g, "-") : null;
 
-                // Menetapkan surah yang dipilih berdasarkan parameter URL
-                $scope.selectedSurah = $scope.ayahs.find(function (ayah) {
-                    return ayah.name.transliteration.id === surahName;
-                });
-
-                // Mengambil data surah yang dipilih
-                var surahData = response.data.data.find(function (s) {
-                    return s.name.transliteration.id === surahName;
-                });
-
-                if (surahData) {
-                    $scope.surah = surahData;
-                } else {
-                    // Redirect ke halaman utama jika surah tidak ditemukan
-                    window.location.href = '/';
-                }
+            $scope.selectedSurah = $scope.ayahs.find(function (ayah) {
+                return ayah.name.transliteration.id === surahName;
             });
 
-            // convert to arabic function
-            $scope.convertToArabic = function(number) {
-                // Fungsi untuk mengonversi angka ke angka Arab
-                const arabicNumbers = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
-                const converted = number.toString().replace(/\d/g, (d) => arabicNumbers[d]);
-                return converted;
-            };
+            var surahData = response.data.data.find(function (s) {
+                return s.name.transliteration.id === surahName;
+            });
 
-              // Fungsi untuk mengarahkan ke halaman surah.html dengan surah yang dipilih dari dropdown
-        // $scope.redirectToSelectedSurah = function () {
-        //     if ($scope.selectedSurah) {
-        //         var surahName = $scope.selectedSurah.name.transliteration.id;
-        //         var url = '/p/al-quran.html?surah=' + surahName;
+            if (surahData) {
+                $scope.surah = surahData;
+            } else {
+                window.location.href = '/';
+            }
+        });
 
-        //         // Mengganti URL dengan parameter yang baru
-        //         $window.location.href = url;
-        //     }
-        // };
+        $scope.convertToArabic = function(number) {
+            const arabicNumbers = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
+            const converted = number.toString().replace(/\d/g, (d) => arabicNumbers[d]);
+            return converted;
+        };
 
         $scope.redirectToSelectedSurah = function (selectedSurah) {
-    if (selectedSurah) {
-        var surahName = selectedSurah.name.transliteration.id;
-        var url = '/p/al-quran.html?surah=' + surahName;
+            if (selectedSurah) {
+                var surahName = selectedSurah.name.transliteration.id;
+                var url = '/p/al-quran.html?surah=' + surahName;
+                $window.location.href = url;
+            }
+        };
 
-        // Redirect to the new URL
-        $window.location.href = url;
-    }
-};
+        $scope.openModal = function (ayah) {
+            $scope.selectedAyah = ayah;
+        };
 
+        var currentAudio;
 
- // Fungsi untuk membuka offcanvas dengan tafsir yang sesuai
- $scope.openModal = function (ayah) {
-        $scope.selectedAyah = ayah;
-    };
-// Fungsi untuk memutar audio
-var currentAudio;
+        $scope.playAudio = function(ayah) {
+            if (currentAudio) {
+                currentAudio.pause();
+                currentAudio.currentTime = 0;
+            }
 
-$scope.playAudio = function(ayah) {
-    if (currentAudio) {
-        currentAudio.pause();
-        currentAudio.currentTime = 0; // Mengembalikan waktu audio ke awal saat dihentikan
-    }
+            if (!ayah.audioPlayer) {
+                ayah.audioPlayer = new Audio(ayah.audio.primary ? ayah.audio.primary : ayah.audio);
+                ayah.audioPlayer.play();
+                currentAudio = ayah.audioPlayer;
+            } else {
+                if (!ayah.audioPlayer.paused) {
+                    ayah.audioPlayer.pause();
+                    ayah.audioPlayer.currentTime = 0;
+                } else {
+                    ayah.audioPlayer.play();
+                    currentAudio = ayah.audioPlayer;
+                }
+            }
+        };
 
-    if (!ayah.audioPlayer) {
-        ayah.audioPlayer = new Audio(ayah.audio.primary ? ayah.audio.primary : ayah.audio);
-        ayah.audioPlayer.play();
-        currentAudio = ayah.audioPlayer;
-    } else {
-        if (!ayah.audioPlayer.paused) {
-            ayah.audioPlayer.pause();
-            ayah.audioPlayer.currentTime = 0; // Mengembalikan waktu audio ke awal saat dihentikan
-        } else {
-            ayah.audioPlayer.play();
-            currentAudio = ayah.audioPlayer;
-        }
-    }
-};
-});
+        // NAVIGATION PREV & NEXT
+        $scope.loadPreviousSurah = function () {
+                var currentIndex = $scope.ayahs.findIndex(function (ayah) {
+                    return ayah.name.transliteration.id === $scope.selectedSurah.name.transliteration.id;
+                });
+
+                if (currentIndex > 0) {
+                    $scope.selectedSurah = $scope.ayahs[currentIndex - 1];
+                    $scope.redirectToSelectedSurah($scope.selectedSurah);
+                }
+
+                $scope.updateSurahNavigation();
+                $scope.$apply(); // Pembaruan tampilan
+            };
+
+            $scope.loadNextSurah = function () {
+                var currentIndex = $scope.ayahs.findIndex(function (ayah) {
+                    return ayah.name.transliteration.id === $scope.selectedSurah.name.transliteration.id;
+                });
+
+                if (currentIndex < $scope.ayahs.length - 1) {
+                    $scope.selectedSurah = $scope.ayahs[currentIndex + 1];
+                    $scope.redirectToSelectedSurah($scope.selectedSurah);
+                }
+
+                $scope.updateSurahNavigation();
+                $scope.$apply(); // Pembaruan tampilan
+            };
+
+            $scope.updateSurahNavigation = function () {
+                var currentIndex = $scope.ayahs.findIndex(function (ayah) {
+                    return ayah.name.transliteration.id === $scope.selectedSurah.name.transliteration.id;
+                });
+
+                $scope.previousSurah = currentIndex > 0 ? $scope.ayahs[currentIndex - 1] : null;
+                $scope.nextSurah = currentIndex < $scope.ayahs.length - 1 ? $scope.ayahs[currentIndex + 1] : null;
+            };
+
+            // Panggil updateSurahNavigation dengan sedikit penundaan menggunakan $timeout
+            $timeout(function () {
+                $scope.updateSurahNavigation();
+            });
+        });
