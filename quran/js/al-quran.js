79271671142d29106e6591697916d790356200f5
sportxyou <143332422+sportxyou@users.noreply.github.com>
2023-11-28 20:18:25 +0700

    Update al-quran.js

--- quran/js/al-quran.js
+++ quran/js/al-quran.js
@@ -14,15 +14,8 @@ var app = angular.module('QuranApp', []);
 
     app.controller('QuranController', function ($scope, $http, $window, $timeout) {
     var urlParams = new URLSearchParams(window.location.search);
-    var surahNameWithDash = urlParams.get('surah');
-    var surahName = surahNameWithDash ? surahNameWithDash.replace(/=/g, "-") : null;
-
-    // Mengabaikan parameter &m=1 dari URL
-    if (urlParams.get('m') === '1') {
-        // Optional: Redirect ke URL tanpa parameter &m=1
-        var newUrl = window.location.href.replace('&m=1', '');
-        $window.history.replaceState({}, document.title, newUrl);
-    }
+        var surahNameWithDash = urlParams.get('surah');
+        var surahName = surahNameWithDash ? surahNameWithDash.replace(/=/g, "-") : null;
         
         $http.get('https://apidataislamic.github.io/data/quran.json').then(function (response) {
             $scope.ayahs = response.data.data; 
@@ -42,8 +35,10 @@ var app = angular.module('QuranApp', []);
                 return s.name.transliteration.id === surahName;
             });
 
-            if (surahData) {
+           if (surahData) {
                 $scope.surah = surahData;
+            } else {
+                window.location.href = '/';
             }
         });
 
